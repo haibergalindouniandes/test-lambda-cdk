61cfce005763911cf8e79771e667741ce128a4ca
Haiber H. Galindo <h.galindos@uniandes.edu.co>
2023-01-24 16:48:57 -0500

    Lamda basica funcional con CDK

--- lib/test-lambda-cdk-stack.ts
+++ lib/test-lambda-cdk-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import { join } from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TestLambdaCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 1) Creacion de la lambda
+    // 2) Enalazamos la lambda a un construct
+    // 3) Una vez configurada nuestra funcion, creamos el template ejecutando en la terminal el comando: cdk synth --o ./templates
+    // 4) Despues de haberse creado el template, realizamos la implementación de la pila del kit de herramientas de CDK en el ambiente AWS: cdk bootstrap
+    // 4) Despues de haberse creado el template, realizamos el deploy con el comando: cdk deploy
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TestLambdaCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Function(this, 'my-lambda-test-user', {
+      runtime: Runtime.NODEJS_18_X,
+      code: Code.fromAsset(join(__dirname, '../lambdas')),
+      handler: 'hello.world'
+    });
+
   }
 }
